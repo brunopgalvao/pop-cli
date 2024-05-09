4e1f09c8f26288f8cbd53efb9bc4a508286be9d8
Bruno Galvao <brunopgalvao@gmail.com>
2024-05-09 16:00:24 +0900

    x9

--- get_latest_release.js
+++ get_latest_release.js
@@ -1,8 +1,9 @@
-const fetch = require('node-fetch');
-const { Octokit } = require('@octokit/rest');
-
 async function getLatestRelease() {
   try {
+    // Use dynamic import for node-fetch
+    const { default: fetch } = await import('node-fetch');
+    const { Octokit } = await import('@octokit/rest');
+
     const octokit = new Octokit({ request: { fetch } });
     const { data: releases } = await octokit.repos.listReleases({
       owner: "r0gue-io",
@@ -10,6 +11,8 @@ async function getLatestRelease() {
     });
     const latestRelease = releases[0];
     const asset = latestRelease.assets.find(asset => asset.name.endsWith("linux-gnu.tar.gz"));
+    
+    // Output the asset URL as a GitHub action output
     console.log(`::set-output name=asset_url::${asset.browser_download_url}`);
   } catch (error) {
     console.error(error);
